4ee1dda2a5dfcf8978fb8adc734bacfc47c9459a
seunghee <victoria9981@naver.com>
2026-07-15 06:27:15 +0900

    build: 라이브러리 카탈로그 및 빌드 설정 정비

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.googleServices) apply false
 }
