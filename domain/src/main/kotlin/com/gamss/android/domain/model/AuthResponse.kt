a0e4c52c96f8c9acc3b38c71d118643b95fcad38
송승희 <victoria9981@naver.com>
2026-07-20 12:53:55 +0900

    fix: domain 모듈 인증 응답 토큰 기본값을 null로 변경

--- domain/src/main/kotlin/com/gamss/android/domain/model/AuthResponse.kt
+++ domain/src/main/kotlin/com/gamss/android/domain/model/AuthResponse.kt
@@ -1,6 +1,6 @@
 package com.gamss.android.domain.model
 
 data class AuthResponse(
-    val accessToken: String = "",
-    val refreshToken: String = "",
+    val accessToken: String? = null,
+    val refreshToken: String? = null,
 )
