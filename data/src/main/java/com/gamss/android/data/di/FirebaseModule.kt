fc7d047119d064195914c73a729570632e720f3f
송승희 <victoria9981@naver.com>
2026-07-31 12:34:01 +0900

    refactor: FirebaseModule을 internal로 변경

--- data/src/main/java/com/gamss/android/data/di/FirebaseModule.kt
+++ data/src/main/java/com/gamss/android/data/di/FirebaseModule.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object FirebaseModule {
+internal object FirebaseModule {
     @Provides
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth = Firebase.auth
