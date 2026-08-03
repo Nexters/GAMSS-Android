30031a776ae93d831d79fbe6fd71e8830ce14d7e
soyeonLee <109227292+soyeonLee126@users.noreply.github.com>
2026-08-03 15:31:24 +0900

    refactor: 에러 코드 헬퍼 파일명을 내용에 맞게 변경

--- data/src/main/java/com/gamss/android/data/repository/ApiErrorCodes.kt
+++ data/src/main/java/com/gamss/android/data/repository/ApiErrorCodes.kt
@@ -3,8 +3,8 @@ package com.gamss.android.data.repository
 import com.gamss.android.core.common.network.ApiException
 
 /**
- * 서버는 "이미 종료됨", "이미 카드 있음"을 409 + error.code 로 알려준다. 이걸 그냥 실패로 흘리면
- * 서버는 성공했는데 클라만 실패로 본 경우(타임아웃 등) 재시도가 영구히 409 라서 복구가 막힌다.
+ * 서버가 준 error.code 로 분기한다. 이 코드들을 그냥 실패로 흘리면, 서버는 성공했는데 클라만
+ * 실패로 본 경우(타임아웃 등) 재시도가 영구히 같은 오류라서 복구가 막힌다.
  */
 internal fun Throwable.hasErrorCode(code: String): Boolean =
     this is ApiException && this.code == code
