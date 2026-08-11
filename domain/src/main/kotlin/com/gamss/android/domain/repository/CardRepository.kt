f64c75eaee3367620cd995960a345b7387f0c670
송승희 <victoria9981@naver.com>
2026-08-11 14:10:24 +0900

    chore:파일 삭제

--- domain/src/main/kotlin/com/gamss/android/domain/repository/CardRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.gamss.android.domain.repository
-
-import com.gamss.android.core.common.AppResult
-import com.gamss.android.domain.card.Card
-
-interface CardRepository {
-    suspend fun getCard(cardId: Long): AppResult<Card>
-}
