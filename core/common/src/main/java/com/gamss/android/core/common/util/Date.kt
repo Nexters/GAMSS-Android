60a2dbcf05abf646444367aa5dbbd12946fae166
seunghee <victoria9981@naver.com>
2026-08-19 02:46:35 +0900

    fix: merge conflict 해결

--- core/common/src/main/java/com/gamss/android/core/common/util/Date.kt
+++ core/common/src/main/java/com/gamss/android/core/common/util/Date.kt
@@ -1,10 +1,8 @@
 package com.gamss.android.core.common.util
 
-import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeParseException
 import java.util.Locale
 
 private const val KOREAN_TIME_PATTERN = "a h:mm"
@@ -23,18 +21,6 @@ private val KoreanWallClockFormatter: DateTimeFormatter =
 // 불교력이나 일본력 로케일에서 연도가 밀리지 않게 ROOT 로 고정한다.
 private val ConversationDateFormatter: DateTimeFormatter =
     DateTimeFormatter.ofPattern(CONVERSATION_DATE_PATTERN, Locale.ROOT)
-
-/**
- * UTC ISO-8601 날짜 문자열을 한국 시간의 `오전/오후 h:mm` 형식으로 변환한다.
- *
- * [isoDateTime]이 ISO-8601 형식이 아니면 `null`을 반환한다.
- */
-fun formatKoreanTime(isoDateTime: String): String? =
-    try {
-        KoreanTimeFormatter.format(Instant.parse(isoDateTime))
-    } catch (_: DateTimeParseException) {
-        null
-    }
 
 /**
  * 이미 벽시계로 바뀐 시각을 `오전/오후 h:mm` 형식으로 변환한다.
